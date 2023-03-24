f1b6988334e496a37600b81fa46ad76ceea5a5c6
Vo Tran Phi <songokupkj@gmail.com>
2023-03-24 20:59:00 +0700

    My Commit

---
+++ algsTimeCalc.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <chrono>
+#include <math.h>
+#include "myAlgs.hpp"
+using namespace std;
+
+int getInteger()
+{
+    return 1000000 + rand()%1000001;
+}
+void generateArray(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = pow(-1, rand() % 2) * rand();
+    }
+}
+void arrayCopy(int *a, int *b, int n) //copy array b[] to array a[].
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = b[i];
+    }
+}
+void menu()
+{
+    cout<<"-----------MENU-----------"<<endl;
+    cout<<"1. Quick Sort."<<endl;
+    cout<<"2. Merge Sort."<<endl;
+    cout<<"3. Binary Insertion Sort."<<endl;
+    cout<<"4. Insertion Sort."<<endl;
+    cout<<"5. Bubble Sort."<<endl;
+    cout<<"6. Selection Sort."<<endl;
+    cout<<"7. Shell Sort."<<endl;
+}
+
+int main()
+{
+    string s;
+    clock_t start, end;
+    short choose;
+    int n = getInteger();
+    int *a = new int [n];
+    int *b = new int [n];
+    generateArray(b, n);
+    
+    do
+    {
+        arrayCopy(a, b, n);
+        menu();
+
+        cout<<"Please choose one algorithm which you want to calculate the processing time: ";
+        cin>>choose;
+        while (choose != 1 && choose != 2 && choose != 3 && choose != 4 && choose != 5 && choose != 6 && choose != 7)
+        {
+            cout<<"There's no algorithm number "<<choose<<". Please choose again: ";
+            cin>>choose;
+        }
+
+        cout<<"Sorting "<<n<<" elements in the array..."<<endl;
+
+        switch (choose)
+        {
+            case 1:
+                start = clock();
+                quickSort(a, 0, n - 1);
+                end = clock();
+                break;
+            case 2:
+                start = clock();
+                mergeSort(a, 0, n - 1);
+                end = clock();
+                break;
+            case 3:
+                start = clock();
+                binaryInsertionSort(a, n);
+                end = clock();
+                break;
+            case 4:
+                start = clock();
+                insertionSort(a, n);
+                end = clock();
+                break;
+            case 5:
+                start = clock();
+                bubbleSort(a, n);
+                end = clock();
+                break;
+            case 6:
+                start = clock();
+                selectionSort(a, n);
+                end = clock();
+                break;
+            case 7:
+                start = clock();
+                shellSort(a, n);
+                end = clock();
+                break;
+        }
+
+        cout<<"Processing time: "<<(double)(end - start) / 1000<<"s"<<endl;
+
+        cout<<"Do you want to continue (type Y or N): ";
+        cin.ignore();
+        getline(cin, s);
+        while (s != "Y" && s != "y" && s != "N" && s != "n")
+        {
+            cout<<"Please type Y or N: ";
+            getline(cin, s);
+        }
+        cout<<endl;
+    }
+    while (s == "y" || s == "Y");
+}
---
+++ myAlgs.hpp
@@ -0,0 +1,223 @@
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void quickSort(int *a, int left, int right)
+{
+    int x = a[(left + right) / 2];
+    int i = left, j = right;
+
+    while (i <= j)
+    {
+        while (a[i] < x)
+        {
+            i++;
+        }
+        while (a[j] > x)
+        {
+            j--;
+        }
+        if (i <= j)
+        {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j)
+    {
+        quickSort(a, left, j);
+    }
+    if (i < right)
+    {
+        quickSort(a, i, right);
+    }
+}
+void merge(int *a, int left, int mid, int right)
+{   
+    int i = left, j = mid + 1, k = left;
+    int *temp = new int [right + 1];
+
+    for (int i = left; i <= right; i++)
+    {
+        temp[i] = a[i];
+    }
+
+    while (i <= mid && j <= right)
+    {
+        if (temp[i] <= temp[j])
+        {
+            a[k++] = temp[i++];
+        }
+        else
+        {
+            a[k++] = temp[j++];
+        }
+    }
+
+    while (i <= mid)
+    {
+        a[k++] = temp[i++];
+    }
+    while (j <= right)
+    {
+        a[k++] = temp[j++];
+    }
+
+    delete[] temp;
+}
+void mergeSort(int *a, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        mergeSort(a, left, mid);
+        mergeSort(a, mid + 1, right);
+        merge(a, left, mid, right);
+    }
+}
+int binarySearch(int *a, int n, int x) //not using recursion
+{
+    int left = 0, right = n - 1;
+    int mid;
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+        if (a[mid] == x)
+        {
+            return mid;
+        }
+        if (a[mid] <= x)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+int binarySearch(int *a, int n, int left, int right, int x) //using recursion
+{
+    if (left > right)
+    {
+        return -1;
+    }
+    int mid = (left + right) / 2;
+    if (a[mid] == x)
+    {
+        return mid;
+    }
+    if (a[mid] <= x)
+    {
+        return binarySearch(a, n, mid + 1, right, x);
+    }
+    else
+    {
+        return binarySearch(a, n, left, mid - 1, x);
+    }
+}
+void insertionSort(int *a, int n)
+{
+    int temp, j;
+    for (int i = 1; i < n; i++)
+    {
+        temp = a[i];
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (temp < a[j])
+            {
+                a[j+1] = a[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        a[j+1] = temp;
+    }
+}
+void binaryInsertionSort(int *a, int n)
+{
+    int x;
+    for (int i = 1; i < n; i++)
+    {
+        x = a[i];
+
+        int left = 0, right = i - 1, mid;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (a[mid] <= x)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        for (int j = i - 1; j >= left; j--)
+        {
+            a[j+1] = a[j];
+        }
+        a[left] = x;
+    }
+}
+void selectionSort(int *a, int n)
+{
+    int minIndex;
+    for (int i = 0; i < n; i++)
+    {
+        minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(a[i], a[minIndex]);
+    }
+}
+void bubbleSort(int *a, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+}
+void shellSort(int *a, int n)
+{
+    int temp, j;
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            temp = a[i];
+            for (j = i; j >= gap; j -= gap)
+            {
+                if (temp < a[j - gap])
+                {
+                    a[j] = a[j - gap];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            a[j] = temp;
+        }
+    }
+}
