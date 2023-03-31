46fa57e2a55e80023805d35dfc62080f7f1f0096
Vo Tran Phi <songokupkj@gmail.com>
2023-04-01 00:51:43 +0700

    My Auto Commit

---
+++ gitAuto.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("cd C:\\Users\\HK\\Desktop\\Code\\OurRepository");
+    system("git add .");
+    system("git commit -m \"My Auto Commit\"");
+    system("git push");
+}
