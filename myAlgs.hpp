32599e813fe2fb60651be103ebabe8a0cd786479
Vo Tran Phi <songokupkj@gmail.com>
2023-04-01 14:22:56 +0700

    My Auto Commit

--- myAlgs.hpp
+++ myAlgs.hpp
@@ -396,3 +396,38 @@ void radixSort(int *a, int n)
         countingSortByDigit(a, n, digitPlace);
     }
 }
+void heapify(int *a, int n, int i)
+{
+    int max = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && a[left] > a[max])
+    {
+        max = left;
+    }
+
+    if (right < n && a[right] > a[max])
+    {
+        max = right;
+    }
+
+    if (max != i)
+    {
+        swap(a[i], a[max]);
+        heapify(a, n, max);
+    }
+}
+void heapSort(int *a, int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(a, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(a[0], a[i]);
+        heapify(a, i, 0);
+    }
+}
