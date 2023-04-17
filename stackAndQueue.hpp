fc24bbbb0202cfa10545d4f6df86f1e031a18294
Vo Tran Phi <songokupkj@gmail.com>
2023-04-17 18:23:15 +0700

    My Auto Commit

--- stackAndQueue.hpp
+++ stackAndQueue.hpp
@@ -61,11 +61,10 @@ void printStack(Stack &top)
         temp = temp->next;
     }
 }
-int top(Stack &top)
+int peek(Stack &top)
 {
     if (isEmpty(top))
     {
-        cout << "Stack is empty!";
         return -1;
     }
     else
@@ -92,5 +91,91 @@ void pop(Stack &top)
 
     top = top->next;
     temp->next = nullptr;
+    delete temp;
+}
+
+
+
+struct Queue
+{
+    int data;
+    Node *front;
+    Node *back;
+};
+
+bool isEmpty(Queue &queue)
+{
+    return queue.front == nullptr && queue.back == nullptr;
+}
+void initiateQueue(Queue &queue)
+{
+    queue.front = queue.back = nullptr;
+}
+void push(Queue &queue, const int &value)
+{
+    Node *newNode = createNode(value);
+
+    if (isEmpty(queue))
+    {
+        queue.front = queue.back = newNode;
+    }
+    else
+    {
+        queue.back->next = newNode;
+        queue.back = newNode;
+    }
+}
+void printStack(Queue &queue)
+{
+    if (isEmpty(queue))
+    {
+        cout << "Queue iss empty!";
+        return;
+    }
+
+    Node *temp = queue.front;
+
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+int front(Queue &queue)
+{
+    if (isEmpty(queue))
+    {
+        return -1;
+    }
+
+    return queue.front->data;
+}
+int back(Queue &queue)
+{
+    if (isEmpty(queue))
+    {
+        return -1;
+    }
+
+    Node *temp = queue.front;
+
+    while (temp != queue.back)
+    {
+        temp = temp->next;
+    }
+
+    return temp->data;
+}
+void pop(Queue &queue)
+{
+    if (isEmpty(queue))
+    {
+        cout << "Queue is empty!";
+        return;
+    }
+
+    Node *temp = queue.front;
+    queue.front = queue.front->next;
+    temp->next =nullptr;
     delete temp;
 }
